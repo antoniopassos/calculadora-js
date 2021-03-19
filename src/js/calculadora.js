b0ec4c83f9560db0d033e91f41de63246501330e
Antonio Passos <antoniopassos@gmail.com>
2021-03-19 13:17:23 -0300

    Implementada a funcionalidade de multiplicação com testes

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -30,7 +30,11 @@ var Calculadora = {
     },
 
     multiplicar: function(num1, num2) {
-        return 0;
+        if (isNaN(num1) || isNaN(num2)) {
+            return 0;
+        }
+
+        return num1 * num2;
     }
 };
 
